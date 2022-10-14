b73ad52347676cc42a8654b9d0b1fad8dff4d584
DevBuster <58188506+DevBuster@users.noreply.github.com>
2022-10-14 09:17:55 -0500

    escuchando eventos con javascript

--- js/index.js
+++ js/index.js
@@ -1,10 +1,36 @@
-function seleccionarJugador() {
-    alert('seleccionado');
+// cargamos el script despues de que el html se cargue
+function cargarJuego() {
+
+    // una forma de llamar a la funcion seleccionarJugador
+    let botonSeleccionarJugador = document.getElementById('boton-seleccionar-jugador');
+    botonSeleccionarJugador.addEventListener('click', seleccionarJugador);
+
 }
 
-// una forma de llamar a la funcion seleccionarJugador
-let botonSeleccionarJugador = document.getElementById('boton-seleccionar-jugador');
-botonSeleccionarJugador.addEventListener('click', seleccionarJugador);
+function seleccionarJugador() {
+
+    if (document.getElementById('radio-jugador-devbuster').checked) {
+
+        alert('Jugador seleccionado: DevBuster');
+
+    } else if (document.getElementById('radio-jugador-ancestralblock').checked) {
+
+        alert('Jugador seleccionado: Ancestral_Block');
+
+    } else if (document.getElementById('radio-jugador-aster').checked) {
+
+        alert('Jugador seleccionado: Aster');
+
+    } else if (document.getElementById('radio-jugador-rex').checked) {
+
+        alert('Jugador seleccionado: Rex');
+
+    } else {
+
+        alert('Seleccione un jugador para continunar');
+
+    }
+}
 
 /* una forma de asignar funcion mediante flecha
     let botonSeleccionarJugador = document.getElementById('boton-seleccionar-jugador');
@@ -12,3 +38,6 @@ botonSeleccionarJugador.addEventListener('click', seleccionarJugador);
             alert('seleccionado');
         });
 */
+
+window.addEventListener('load', cargarJuego);
+// window.addEventListener('DOMContentLoaded', cargarJuego);
